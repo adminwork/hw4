e48b4df765ed3cbb2cbb033cd39e95ead8ada506
Ivan <korniyenko.ivan@gmail.com>
2015-06-03 10:09:36 +0300

    4-8

--- index4_1.php
+++ index4_1.php
@@ -3,7 +3,7 @@
 
 function calc($str)
 {
-    $str = str_replace(array('_', '-', '—', '(', ')', '“', '”', '«', '»', '.', ':', '+', '/'), ' ', trim($str));
+//    $str = str_replace(array('_', '-', '—', '(', ')', '“', '”', '«', '»', '.', ':', '+', '/'), ' ', trim($str));
 //    $str=preg_replace('  ', ' ', $str);
     //   var_dump($str);
     //    var_dump($a);
@@ -18,9 +18,9 @@ function calc($str)
 //
 //    array_walk($arr, 'trim_value');
     //    var_dump($arr);
-//    preg_match('#Преподаватели Source IT(.*)Полный перечень наших курсов:#is', $string, $matches);
+    preg_match('#Преподаватели Source IT(.*)Полный перечень наших курсов:#is', $string, $matches);
     //    var_dump($matches);
-//    $arr   = str_word_count($matches[1],2,"АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя«»“”ä");
+    $arr   = str_word_count($matches[1],2,"АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя«»“”ä");
     //    print_r($c);
     $result = count($arr);              //количество слов является количество элементов массива
     echo 'Количество слов:', $result;
@@ -29,31 +29,31 @@ function calc($str)
     $tmp = array_count_values($arr);
     $max = max($tmp);
     $result = array_search($max, $tmp);
-    echo 'Самый повторяющийся элемент массива: ', $result, '. Он повторяется ', $max, ' раз(а).';
-    echo "<br>";
+        echo 'Самый повторяющийся элемент массива: ', $result, '. Он повторяется ', $max, ' раз(а).';
+        echo "<br>";
     //var_dump($tmp);
     $min = min($tmp);
     //var_dump($min);
-    echo 'Самые редкие слова, повторяются ' . "$min" . ' раз:';
+        echo 'Самые редкие слова, повторяются ' . "$min" . ' раз:';
     $tmpmin = array_flip($tmp);
-    foreach ($tmpmin as $key => $value) {
-        if ($key = $min) ;
-        $arrmin = $value;
-        echo "$arrmin ,";
-    }
+        foreach ($tmpmin as $key => $value) {
+            if ($key = $min) ;
+            $arrmin = $value;
+            echo "$arrmin ,";
+        }
 
     echo '<table border="1" cellpadding="5">';   //таблица слов с кол-вом повторений
     echo "<tr>";
     echo "<td><b>Слово</b></td>";
     echo "<td><b>Количество</b></td>";
     echo "</tr>";
-    foreach ($tmp as $key => $value) {
-        echo "
-            <tr>
-                <td>$key</td>
-                <td>$value</td>
-            </tr>";
-    }
+        foreach ($tmp as $key => $value) {
+            echo "
+                <tr>
+                    <td>$key</td>
+                    <td>$value</td>
+                </tr>";
+        }
     echo "</table>";
 }
 
@@ -61,11 +61,7 @@ $string = file_get_contents('http://source-it.com.ua/teachers/');
 $string = strip_tags($string);
 
 $pozica = mb_strpos($string,"Татьяна Муращенко");  //находим позицию начального элемента
-//    echo $v;
-//    var_dump($v);
 $pozicb = mb_strpos($string,"Полный перечень наших курсов:"); //находим позицию конечного элемента
-//    echo $f;
-//    var_dump($f);
 $text = $pozicb - $pozica;
 $str = mb_substr($string,$pozica,$text);      //вычисляем кусок текста
 
